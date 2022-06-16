5403de2b400ae7f48d4c147006a7c0831f7f946f
Dylan Gonzales <dylancartroll@hotmail.com>
2022-06-15 23:34:57 -0500

    webhash

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AdminView.vue'
 import UserView from '../views/UserView.vue'
@@ -50,7 +50,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(process.env.BASE_URL),
   routes
 })
 
